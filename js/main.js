b38e882249b3da977e9e8f69ec8fb7ac0c69731f
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-05-08 16:03:00 +0200

    random number function

--- js/main.js
+++ js/main.js
@@ -33,17 +33,26 @@
 // Pari e Dispari
 
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+
+let userChoice = prompt('scegli se il risultato sarà pari o dispari')
+console.log("l'utente ha scelto " + userChoice);
+
 let userNumber = parseInt(prompt('inserisci un numero da 1 a 5'))
 console.log("il numero dell'utente è " + userNumber);
 
+
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 
-let computerNumber = Math.floor(Math.random() * 5) + 1;
-console.log("il numero del computer è " + computerNumber);
+let computerNumber = generateComputerNumber()
+console.log('il numero scelto dal pc è ' + computerNumber);
 
+function generateComputerNumber() {
+    let num = Math.floor(Math.random() * 5) + 1;
 
+    return (num);
+}
 
-// Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+// Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) 
 
 let somma = userNumber + computerNumber;
 console.log('la somma dei numeri è ' + somma);
@@ -54,3 +63,5 @@ console.log('il numero è pari');
 else{
     console.log('la somma dei numeri è dispari');
 }
+
+// Dichiariamo chi ha vinto.
