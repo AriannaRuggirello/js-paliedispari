cbcb396ebf2b9b9152b5aafc1687d7ed86eb4014
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-05-08 17:48:18 +0200

    indentazione

--- js/main.js
+++ js/main.js
@@ -1,52 +1,47 @@
-// // PAROLA PALINDROMA
-// console.log('PAROLA PALINDROMA');
-// // Chiedere all’utente di inserire una parola
-// let word = prompt('inserisci una parola ');
+// PAROLA PALINDROMA
+console.log('PAROLA PALINDROMA');
 
+// Chiedere all’utente di inserire una parola
+let word = prompt('inserisci una parola ');
+let check = isPalindrome(word);
 
-// let check = isPalindrome(word);
-// document.getElementById('risultato');
-
-// // Creare una funzione per capire se la parola inserita è palindroma
-
-// function isPalindrome(word) {
+// Creare una funzione per capire se la parola inserita è palindroma
+function isPalindrome(word) {
    
-//     // trasformo la parola in un array di lettere
-//     var letters = word.split("");
-//     console.log(letters);
+    // trasformo la parola in un array di lettere
+    var letters = word.split("");
+    console.log(letters);
 
-//     // creo un array che contiene le stesse lettere 
-//     var reversedLetters = letters.reverse();
-//     console.log(reversedLetters);
+    // creo un array che contiene le stesse lettere 
+    var reversedLetters = letters.reverse();
+    console.log(reversedLetters);
 
-//     // riunisco le lettere
-//     var reversedLetters = letters.join('');
-//     console.log(reversedLetters);
+    // riunisco le lettere
+    var reversedLetters = letters.join('');
+    console.log(reversedLetters);
 
-//     // confronto la parola iniziale con quella invertita
-//     if(word == reversedLetters){
-//         console.log('la parola è palindroma') ;
-//     } else {
-//         console.log('la parola NON è palindroma') ;
-//     }
+    // confronto la parola iniziale con quella invertita
+    if(word == reversedLetters){
+        console.log('la parola è palindroma') ;
+    } else {
+        console.log('la parola NON è palindroma') ;
+    }
   
-//   }
+  }
   
 
 // PARI E DISPARI
 console.log('PARI O DISPARI');
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 
+// L’utente sceglie pari o dispari
 let userChoice = prompt('scegli se il risultato sarà pari o dispari')
 console.log("l'utente ha scelto " + userChoice);
 
-
+// L’utente inserisce un numero da 1 a 5
 let userNumber = parseInt(prompt('inserisci un numero da 1 a 5'))
 console.log("il numero dell'utente è " + userNumber);
 
-
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-
 let computerNumber = generateComputerNumber(1,5)
 console.log('il numero scelto dal pc è ' + computerNumber);
 
@@ -57,11 +52,8 @@ function generateComputerNumber(min,max) {
 }
 
 // Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) 
-
 let solution = sum(userNumber,computerNumber);
 console.log(solution);
-
-
 
 function sum(userNumber,computerNumber) 
 {
