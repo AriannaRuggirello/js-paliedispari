3db2c3b639832d031973a7780c423937b1639706
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-05-08 15:45:47 +0200

    pari e dispari draft

--- js/main.js
+++ js/main.js
@@ -1,46 +1,56 @@
-// Palidroma
+// // Palidroma
 
-// Chiedere all’utente di inserire una parola
-let word = prompt('inserisci una parola ');
-let check = isPalindrome(word);
+// // Chiedere all’utente di inserire una parola
+// let word = prompt('inserisci una parola ');
+// let check = isPalindrome(word);
 
-// Creare una funzione per capire se la parola inserita è palindroma
+// // Creare una funzione per capire se la parola inserita è palindroma
 
-function isPalindrome(word) {
+// function isPalindrome(word) {
    
-    // trasformo la parola in un array di lettere
-    var letters = word.split("");
-    console.log(letters);
+//     // trasformo la parola in un array di lettere
+//     var letters = word.split("");
+//     console.log(letters);
 
-    // creo un array che contiene le stesse lettere 
-    var reversedLetters = letters.reverse();
-    console.log(reversedLetters);
+//     // creo un array che contiene le stesse lettere 
+//     var reversedLetters = letters.reverse();
+//     console.log(reversedLetters);
 
-    // riunisco le lettere
-    var reversedLetters = letters.join('');
-    console.log(reversedLetters);
+//     // riunisco le lettere
+//     var reversedLetters = letters.join('');
+//     console.log(reversedLetters);
 
-    // confronto la parola iniziale con quella invertita
-    if(word == reversedLetters){
-        console.log('la parola è palindroma') ;
-    } else {
-        console.log('la parola non è palindroma') ;
-    }
+//     // confronto la parola iniziale con quella invertita
+//     if(word == reversedLetters){
+//         console.log('la parola è palindroma') ;
+//     } else {
+//         console.log('la parola NON è palindroma') ;
+//     }
   
-  }
+//   }
   
-
-
-
-
-
-
-
-
-
-
 
 // Pari e Dispari
+
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+let userNumber = parseInt(prompt('inserisci un numero da 1 a 5'))
+console.log("il numero dell'utente è " + userNumber);
+
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+
+let computerNumber = Math.floor(Math.random() * 5) + 1;
+console.log("il numero del computer è " + computerNumber);
+
+
+
 // Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+let somma = userNumber + computerNumber;
+console.log('la somma dei numeri è ' + somma);
+
+if(somma % 2 === 0){
+console.log('il numero è pari');
+}
+else{
+    console.log('la somma dei numeri è dispari');
+}
